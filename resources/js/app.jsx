27f7f56871baf831b9252889013a8b39f2749bc0
Kusumlimb <kusumlimbu75@gmail.com>
2024-07-10 11:57:22 +0545

    initial commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,16 @@
+import './bootstrap';
+import '../css/app.css';
+import React from "react"
+import { createRoot } from "react-dom/client"
+import AppRouter from './Router';
+
+const App = () => {
+    return(
+        <React.StrictMode>
+            <AppRouter/>
+        </React.StrictMode>
+    )
+}
+
+const root = createRoot(document.getElementById("app"));
+root.render(<App/>);
