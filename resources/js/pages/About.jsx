2eaac5bb1336b18b0a01f0b7925f214d54a6efea
Kusumlimb <kusumlimbu75@gmail.com>
2024-07-15 13:10:41 +0545

    small description change

--- resources/js/pages/About.jsx
+++ resources/js/pages/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
       <h1>About Me</h1>
       <p>I am Kusum Limbu. I am currently pursuing my bachelor's degree in Itahari International college. 
         The name of my degree is Bsc Hons Computing. This degree  is actually provided by the London Metropolitan university of London.
-        My hobbies are playing volleyball and performing.
+        My hobbies are playing volleyball and dancing.
       </p>
     </div>
         </HomeLayout>
